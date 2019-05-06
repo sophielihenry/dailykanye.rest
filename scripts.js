07a1105f178cf9604048b14205db89059404b6bb
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-05-06 15:04:12 +0100

    Adding styling with javascript

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 fetch('https://api.kanye.rest')
  .then(response => response.json())
  .then (data => {
-    console.log(data)
+    console.log(data.quote)
  })
  .catch(error => console.error(error))
+
+const app = document.getElementById('root')
+const quoteContainer = document.createElement("div")
+quoteContainer.setAttribute('class', 'container')
+
+app.appendChild(quoteContainer)
