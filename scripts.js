11840a422d806f66d043f4cb3a9066f720112c2c
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-05-06 15:06:22 +0100

    quotes now display on index page

--- scripts.js
+++ scripts.js
@@ -2,11 +2,12 @@ fetch('https://api.kanye.rest')
  .then(response => response.json())
  .then (data => {
     console.log(data.quote)
+    const app = document.getElementById('root')
+    const quoteContainer = document.createElement("div")
+    quoteContainer.setAttribute('class', 'quoteContainer')
+    quoteContainer.innerHTML = data.quote
+
+    app.appendChild(quoteContainer)
  })
  .catch(error => console.error(error))
 
-const app = document.getElementById('root')
-const quoteContainer = document.createElement("div")
-quoteContainer.setAttribute('class', 'container')
-
-app.appendChild(quoteContainer)
