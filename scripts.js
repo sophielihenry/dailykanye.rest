8473694e0bb72c5c1d88c626fc5eca279eeb574d
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-05-06 16:01:02 +0100

    can load new quote with API call however it doesnt replace it just adds loads to pgae

--- scripts.js
+++ scripts.js
@@ -1,20 +1,27 @@
-fetch('https://api.kanye.rest')
- .then(response => response.json())
- .then (data => {
-    // console.log(data.quote)
-    const app = document.getElementById('root')
-    const quoteContainer = document.createElement("div")
-    quoteContainer.setAttribute('class', 'container')
-    quoteContainer.innerHTML = data.quote
-    const button = document.createElement("button")
-    button.textContent = "another"
-    app.appendChild(quoteContainer)
-    app.appendChild(button)
+function getQuote() {
+  fetch('https://api.kanye.rest')
+   .then(response => response.json())
+   .then (data => {
+      // console.log(data.quote)
+      const app = document.getElementById('root')
+      const quoteContainer = document.createElement("div")
+      quoteContainer.setAttribute('class', 'container')
+      quoteContainer.innerHTML = data.quote
+      const button = document.createElement("button")
+      button.textContent = "another"
+      app.appendChild(quoteContainer)
+      app.appendChild(button)
+button.addEventListener('click', getQuote)
 
-    button.addEventListener('click', function(event) {
-      console.log(event)
-    })
+   })
+   .catch(error => console.error(error))
+}
 
- })
- .catch(error => console.error(error))
 
+getQuote()
+
+
+
+// const newQuote = document.getElementById('newQuote')
+// newQuote.addEventListener('click', getQuote); // new quote on button click
+// window.onload = getQuote; // new quote on page load
