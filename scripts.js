213a275a229773bbbe5babbaaed270a038b5e384
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-05-06 15:14:43 +0100

    git test as renamed repo

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
 fetch('https://api.kanye.rest')
  .then(response => response.json())
  .then (data => {
-    console.log(data.quote)
+    // console.log(data.quote)
     const app = document.getElementById('root')
     const quoteContainer = document.createElement("div")
-    quoteContainer.setAttribute('class', 'quoteContainer')
+    quoteContainer.setAttribute('class', 'container')
     quoteContainer.innerHTML = data.quote
-
+    const button = document.createElement("button")
+    button.textContent = "another"
     app.appendChild(quoteContainer)
+    app.appendChild(button)
+
+    button.addEventListener('click', function(event) {
+
+      console.log(event)
+
+    })
+
+
  })
  .catch(error => console.error(error))
 
